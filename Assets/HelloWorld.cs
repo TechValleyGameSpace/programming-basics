160c9325b6f47963bdf057673ef4fba47c282fba
Taro Omiya <taro.omiya@omiyagames.com>
2015-06-18 01:43:36 +0000

    End produce to tutorial.

---
+++ Assets/HelloWorld.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelloWorld : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+        int GHKLDF = 8;
+
+
+
+        float side1 = 7f;
+        float side2 = 2f;
+        float hypotenuse = side1 - side2;
+        Debug.Log(hypotenuse);
+    }
+}
