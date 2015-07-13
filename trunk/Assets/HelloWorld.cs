efe5fdfee07301ce77bb971a5a304e89a5a74787
tomiya <tomiya@852bb225-69ad-480d-8cc9-601e2e7becdf>
2015-07-13 21:41:17 +0000

    Working on Part 2.  Upgrading to Unity 5.1

--- trunk/Assets/HelloWorld.cs
+++ trunk/Assets/HelloWorld.cs
@@ -2,9 +2,16 @@
 using System.Collections;
 
 public class HelloWorld : MonoBehaviour {
+	public bool isTrue = true;
+	public int intNum = 1;
+	public float floatNum = 2f;
+	public string word = "Za Waludo";
+
 
 	// Use this for initialization
 	void Start () {
-        Debug.Log(1);
+		Debug.Log(typeof(bool));
+		string x = "Hello World!";
+		Debug.Log(x.Replace("Hello", "Goodbye"));
 	}
 }
