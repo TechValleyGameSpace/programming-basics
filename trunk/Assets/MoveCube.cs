7935fdb06ae616c2b36ae6317662322c70803523
tomiya <tomiya@852bb225-69ad-480d-8cc9-601e2e7becdf>
2015-07-13 22:09:57 +0000

    Putting together events for Part 2 of basics of programming.

---
+++ trunk/Assets/MoveCube.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCube : MonoBehaviour {
+	public Vector3 speed;
+	void Update() {
+		Vector3 pos = transform.position;
+		pos += speed * Time.deltaTime;
+		transform.position = pos;
+	}
+}
